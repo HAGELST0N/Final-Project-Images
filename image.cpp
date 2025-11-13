59417739706f670d3ca620da88cc1ab070de6024
HAGELST0N <patrick.hagelston@gmail.com>
2025-11-13 11:56:39 -0500

    worked on image.h and image.cpp files

--- image.cpp
+++ image.cpp
@@ -1,16 +1,14 @@
 #include "image.h"
 using namespace std;
 
-void displayNoise(const int imgWidth, const int imgHeight, vector<int> inputImgVec)
+void displayImg(const int imgWidth, const int imgHeight, vector<int> inputImgVec)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "Perlin Noise");
 
-    SetTargetFPS(60);
+    SetTargetFPS(5);
 
     Image img = GenImageColor(imgHeight, imgHeight, WHITE);
     ImageFormat(&img, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
--- image.h
+++ image.h
@@ -7,6 +7,6 @@ using namespace std;
 #ifndef HELLORAYLIB_IMAGE_H
 #define HELLORAYLIB_IMAGE_H
 
-void displayNoise(int imgWidth, int imgHeight, vector<int> noise);
+void displayImg(int imgWidth, int imgHeight, vector<int> noise);
 
 #endif //HELLORAYLIB_IMAGE_H

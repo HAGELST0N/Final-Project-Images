dbea748d7e4e593b922c05142589b118b1cbb44b
HAGELST0N <patrick.hagelston@gmail.com>
2025-11-13 12:05:20 -0500

    Adjusted displayImg function

--- image.h
+++ image.h
@@ -7,6 +7,6 @@ using namespace std;
 #ifndef HELLORAYLIB_IMAGE_H
 #define HELLORAYLIB_IMAGE_H
 
-void displayImg(int imgWidth, int imgHeight, vector<int> noise);
+void displayImg(int imgWidth, int imgHeight, vector<int> noise, Color* pixels);
 
 #endif //HELLORAYLIB_IMAGE_H

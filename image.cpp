a18869928353423e450ae1c60a5a3e20105fe287
HAGELST0N <patrick.hagelston@gmail.com>
2025-11-13 11:44:47 -0500

    worked on image.h and image.cpp files

--- image.cpp
+++ image.cpp
@@ -1,1 +1,47 @@
 #include "image.h"
+using namespace std;
+
+void displayNoise(const int imgWidth, const int imgHeight, vector<int> inputImgVec)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Perlin Noise");
+
+    SetTargetFPS(60);
+
+    Image img = GenImageColor(imgHeight, imgHeight, WHITE);
+    ImageFormat(&img, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
+    auto pixels = (Color*)img.data;
+    Texture tex = LoadTextureFromImage(img);
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // your update logic goes here
+        for(int row = 0; row < imgHeight; row++)
+        {
+            for(int col = 0; col < imgWidth; col++)
+            {
+                pixels[row * imgWidth + col] = (Color) {
+                    (uint8_t)inputImgVec[row * imgWidth + col],
+                    (uint8_t)inputImgVec[row * imgWidth + col],
+                    (uint8_t)inputImgVec[row * imgWidth + col],
+                    255
+                };
+            }
+        }
+        UpdateTexture(tex, pixels);
+
+        // drawing logic goes here
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawTexture(tex, 200, 25, WHITE);
+        EndDrawing();
+    }
+
+    UnloadTexture(tex);
+
+    CloseWindow();
+}
--- image.h
+++ image.h
@@ -1,10 +1,12 @@
 #include <raylib.h>
 #include <raygui.h>
 #include <cstdint>
+#include "Noise.h"
+using namespace std;
 
 #ifndef HELLORAYLIB_IMAGE_H
 #define HELLORAYLIB_IMAGE_H
 
-void displayNoise(width, height, noise){}
+void displayNoise(int imgWidth, int imgHeight, vector<int> noise);
 
 #endif //HELLORAYLIB_IMAGE_H

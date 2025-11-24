b0adebfeb3832fd2e57da3d0f12fcda4d8c7bfd0
MistyPyro <cskaarup05@gmail.com>
2025-11-24 18:16:47 -0500

    added for loop to color in traveled distances

--- Worm.cpp
+++ Worm.cpp
@@ -28,6 +28,10 @@ void Worm::Walk()
         auto direction = getDirection(h_xPos, h_yPos);
         float new_xPos = h_xPos + direction.first*speed;
         float new_yPos = h_yPos + direction.second*speed;
+        //push back all values between previous and new positions
+        for (int i = int(h_xPos); i < int(new_xPos); i++) {
+            positions.push_back(std::make_pair(i, (new_yPos - h_yPos) / (new_xPos - i) ));
+        }
         positions.push_back(std::make_pair(new_xPos, new_yPos));
         h_xPos = new_xPos;
         h_yPos = new_yPos;

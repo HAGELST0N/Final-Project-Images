205967d61c34ef98ab3cec1c0be4e0f0ebcf1726
MistyPyro <cskaarup05@gmail.com>
2025-11-27 16:17:13 -0500

    modified math in getDirection() and Walk()

--- Worm.cpp
+++ Worm.cpp
@@ -15,9 +15,10 @@ Worm::Worm() {
 }
 
 std::pair<float, float> Worm::getDirection(float x, float y) {
-    float degrees = noise.GetNoise(x, y);
-    x = cos(M_PI * (degrees));
-    y = sin(M_PI * (degrees));
+    float noiseAmt = noise.GetNoise(x, y);
+    float degrees = (noiseAmt + 1) * 180;
+    x = cos(degrees);
+    y = sin(degrees);
     return std::make_pair(x, y);
 }
 
@@ -28,9 +29,30 @@ void Worm::Walk()
         auto direction = getDirection(h_xPos, h_yPos);
         float new_xPos = h_xPos + direction.first*speed;
         float new_yPos = h_yPos + direction.second*speed;
+        // for (int i = int(h_xPos); i < abs(int(new_xPos - h_xPos)); i++) {
+        //     positions.push_back(std::make_pair(i, (new_yPos - h_yPos) * ((float)i / (new_yPos - h_yPos))/*(new_yPos - h_yPos) / (new_xPos - i) */));
+        // }
         //push back all values between previous and new positions
-        for (int i = int(h_xPos); i < int(new_xPos); i++) {
-            positions.push_back(std::make_pair(i, (new_yPos - h_yPos) * ((float)i / (new_yPos - h_yPos))/*(new_yPos - h_yPos) / (new_xPos - i) */));
+        //adds or subtracts from h_Pos based on if distance is negative or positive
+        if (new_xPos - h_xPos >= 0 && new_yPos - h_yPos >= 0) {
+            for (int i = 1; i < abs(int(new_xPos - h_xPos)); i++) {
+                positions.push_back(std::make_pair(h_xPos + ((new_xPos - h_xPos) * ((float)i / (new_xPos - h_xPos))), h_yPos + ((new_yPos - h_yPos) * ((float)i / (new_yPos - h_yPos)))));
+            }
+        }
+        else if (new_xPos - h_xPos < 0 && new_yPos - h_yPos >= 0) {
+            for (int i = 1; i < abs(int(new_xPos - h_xPos)); i++) {
+                positions.push_back(std::make_pair(h_xPos - ((new_xPos - h_xPos) * ((float)i / (new_xPos - h_xPos))), h_yPos + ((new_yPos - h_yPos) * ((float)i / (new_yPos - h_yPos)))));
+            }
+        }
+        else if (new_xPos - h_xPos >= 0 && new_yPos - h_yPos < 0) {
+            for (int i = 1; i < abs(int(new_xPos - h_xPos)); i++) {
+                positions.push_back(std::make_pair(h_xPos + ((new_xPos - h_xPos) * ((float)i / (new_xPos - h_xPos))), h_yPos - ((new_yPos - h_yPos) * ((float)i / (new_yPos - h_yPos)))));
+            }
+        }
+        else {
+            for (int i = 1; i < abs(int(new_xPos - h_xPos)); i++) {
+                positions.push_back(std::make_pair(h_xPos - ((new_xPos - h_xPos) * ((float)i / (new_xPos - h_xPos))), h_yPos - ((new_yPos - h_yPos) * ((float)i / (new_yPos - h_yPos)))));
+            }
         }
         positions.push_back(std::make_pair(new_xPos, new_yPos));
         h_xPos = new_xPos;
